b6b25e6d931464deb5198e63a364f5fadb2e00cb
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-03 18:47:36 -0300

    Add bank fields required by pagarme on Partner cpt

--- includes/cpts/CustomPostTypePartner.php
+++ includes/cpts/CustomPostTypePartner.php
@@ -16,7 +16,49 @@ class CustomPostTypePartner extends CustomPostType {
                     'number', 
                     'psp_recipient_id', 
                     __('Pagar.me Recipient ID')
-                )
+                ),
+                Field::make(
+                    'number', 
+                    'psp_bank_code', 
+                    __('Bank Code')
+                ),
+                Field::make(
+                    'number', 
+                    'psp_agency', 
+                    __('Agency number')
+                )->set_width(50),
+                Field::make(
+                    'number', 
+                    'psp_agency_digit', 
+                    __('Agency digit')
+                )->set_width(50),
+                Field::make(
+                    'number', 
+                    'psp_account', 
+                    __('Account number')
+                )->set_width(50),
+                Field::make(
+                    'number', 
+                    'psp_account_digit', 
+                    __('Account digit')
+                )->set_width(50),
+                Field::make(
+                    'select', 
+                    'psp_account_type', 
+                    __('Account type')
+                )->add_options([
+                    'conta_corrente' => __('Current account'),
+                ])->set_width(50),
+                Field::make(
+                    'number', 
+                    'psp_document_number', 
+                    __('Document number')
+                )->set_width(50),
+                Field::make(
+                    'text', 
+                    'psp_legal_name', 
+                    __('Legal name')
+                ),
             ]
         );
     }
