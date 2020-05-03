8120b24059f42c87cb5f432b90ceeb38f5010965
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-03 16:04:40 -0300

    Improve class indentation

--- includes/cpts/CustomPostTypeProduct.php
+++ includes/cpts/CustomPostTypeProduct.php
@@ -13,26 +13,30 @@ class CustomPostTypeProduct extends CustomPostType
             'Product',
             'product',
             [
-                Field::make('complex', 'psp_partners', __('Partners payment split'))
-                    ->add_fields([
-                        Field::make(
-                            'association',
-                            'psp_partner',
-                            __('Partner')
-                        )->set_types([
-                            [
-                                'type' => 'post',
-                                'post_type' => 'partner'
-                            ]
-                        ])->set_min(1)
-                        ->set_max(1)
-                        ->set_width(50),
-                        Field::make(
-                            'number',
-                            'psp_percentage',
-                            __('Partner percentage')
-                        )->set_width(50)
-                    ])
+                Field::make(
+                    'complex', 
+                    'psp_partners', 
+                    __('Partners payment split')
+                )
+                ->add_fields([
+                    Field::make(
+                        'association',
+                        'psp_partner',
+                        __('Partner')
+                    )->set_types([
+                        [
+                            'type' => 'post',
+                            'post_type' => 'partner'
+                        ]
+                    ])->set_min(1)
+                    ->set_max(1)
+                    ->set_width(50),
+                    Field::make(
+                        'number',
+                        'psp_percentage',
+                        __('Partner percentage')
+                    )->set_width(50)
+                ])
             ],
             true
         );
