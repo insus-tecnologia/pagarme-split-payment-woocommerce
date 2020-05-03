0943cd7745857d3143e8a14dfa6052e20bc33f05
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-03 13:33:30 -0300

    Initialize repository

---
+++ pagarme-split-payment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Pagar.me Split Payment for WooCommerce
+ * Description: Allow you to define partners to split payment with using Pagar.me gateway.
+ * Version: 1.0.0
+ * Author: Raphael Batagini
+ * Author URI: https://www.linkedin.com/in/raphael-batagini/
+ * Text Domain: pagarme-split-woocommerce
+ * Domain Path: /i18n/languages/
+ *
+ * @package PagarmeSplitWooCommerce
+ */
