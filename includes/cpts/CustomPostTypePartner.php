3e2c63a51c844cd62a80e282cf128478ca4c78a5
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-05-06 22:47:58 -0300

    Put partners fields inside a group to get they at once

--- includes/cpts/CustomPostTypePartner.php
+++ includes/cpts/CustomPostTypePartner.php
@@ -12,71 +12,74 @@ class CustomPostTypePartner extends CustomPostType {
             'Partner',
             'partner',
             [
-                Field::make(
-                    'text', 
-                    'psp_recipient_id', 
-                    __('Pagar.me Recipient ID')
-                )->set_attributes([
-                    'readOnly' => 'readOnly',
-                    'placeholder' => __('This field will be filled by Pagar.me')
-                ]),
-                Field::make(
-                    'text', 
-                    'psp_bank_code', 
-                    __('Bank Code')
-                )->set_attribute('type', 'number')
-                ->set_required(true),
-                Field::make(
-                    'text', 
-                    'psp_agency', 
-                    __('Agency number')
-                )->set_width(50)
-                ->set_attribute('type', 'number')
-                ->set_required(true),
-                Field::make(
-                    'text', 
-                    'psp_agency_digit', 
-                    __('Agency digit')
-                )->set_width(50)
-                ->set_attribute('type', 'number'),
-                Field::make(
-                    'text', 
-                    'psp_account', 
-                    __('Account number')
-                )->set_width(50)
-                ->set_attribute('type', 'number')
-                ->set_required(true),
-                Field::make(
-                    'text', 
-                    'psp_account_digit', 
-                    __('Account digit')
-                )->set_width(50)
-                ->set_attribute('type', 'number')
-                ->set_required(true),
-                Field::make(
-                    'select', 
-                    'psp_account_type', 
-                    __('Account type')
-                )->add_options([
-                    'conta_corrente' => __('Current account'),
-                    'conta_poupanca' => __('Savings account'), 
-                    'conta_corrente_conjunta' => __('Joint current account'), 
-                    'conta_poupanca_conjunta' => __('Joint savings account'),
-                ])->set_width(50)
-                ->set_required(true),
-                Field::make(
-                    'text', 
-                    'psp_document_number', 
-                    __('Document number')
-                )->set_width(50)
-                ->set_attribute('type', 'number')
-                ->set_required(true),
-                Field::make(
-                    'text', 
-                    'psp_legal_name', 
-                    __('Legal name')
-                )->set_attribute('maxLength', 30)
-                ->set_required(true),
+                Field::make('complex', 'psp_partner')->add_fields([
+                    Field::make(
+                        'text',
+                        'psp_recipient_id',
+                        __('Pagar.me Recipient ID')
+                    )->set_attributes([
+                        'readOnly' => 'readOnly',
+                        'placeholder' => __('This field will be filled by Pagar.me')
+                    ]),
+                    Field::make(
+                        'text',
+                        'psp_bank_code',
+                        __('Bank Code')
+                    )->set_attribute('type', 'number')
+                        ->set_required(true),
+                    Field::make(
+                        'text',
+                        'psp_agency',
+                        __('Agency number')
+                    )->set_width(50)
+                        ->set_attribute('type', 'number')
+                        ->set_required(true),
+                    Field::make(
+                        'text',
+                        'psp_agency_digit',
+                        __('Agency digit')
+                    )->set_width(50)
+                        ->set_attribute('type', 'number'),
+                    Field::make(
+                        'text',
+                        'psp_account',
+                        __('Account number')
+                    )->set_width(50)
+                        ->set_attribute('type', 'number')
+                        ->set_required(true),
+                    Field::make(
+                        'text',
+                        'psp_account_digit',
+                        __('Account digit')
+                    )->set_width(50)
+                        ->set_attribute('type', 'number')
+                        ->set_required(true),
+                    Field::make(
+                        'select',
+                        'psp_account_type',
+                        __('Account type')
+                    )->add_options([
+                        'conta_corrente' => __('Current account'),
+                        'conta_poupanca' => __('Savings account'),
+                        'conta_corrente_conjunta' => __('Joint current account'),
+                        'conta_poupanca_conjunta' => __('Joint savings account'),
+                    ])->set_width(50)
+                        ->set_required(true),
+                    Field::make(
+                        'text',
+                        'psp_document_number',
+                        __('Document number')
+                    )->set_width(50)
+                        ->set_attribute('type', 'number')
+                        ->set_required(true),
+                    Field::make(
+                        'text',
+                        'psp_legal_name',
+                        __('Legal name')
+                    )->set_attribute('maxLength', 30)
+                        ->set_required(true),
+                ])->set_min(1)
+                ->set_max(1)
             ]
         );
     }
