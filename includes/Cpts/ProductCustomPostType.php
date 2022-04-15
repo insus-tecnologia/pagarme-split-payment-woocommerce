df68dccddf0747b961f22cc9fe4233567f510558
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-04-15 16:49:28 -0300

    implements partner comission validation

--- includes/Cpts/ProductCustomPostType.php
+++ includes/Cpts/ProductCustomPostType.php
@@ -2,7 +2,10 @@
 
 namespace PagarmeSplitPayment\Cpts;
 
+use Carbon_Fields\Field\Field;
+use PagarmeSplitPayment\Entities\Partner;
 use PagarmeSplitPayment\Fields\PartnersFieldGroup;
+use PagarmeSplitPayment\Helper;
 
 class ProductCustomPostType extends CustomPostType
 {
@@ -15,5 +18,30 @@ class ProductCustomPostType extends CustomPostType
             PartnersFieldGroup::get(),
             true
         );
+
+        add_action('save_post_product', [$this, 'validatePartnersComission'], 10);
+    }
+
+    public function validatePartnersComission(int $postID)
+    {
+        $partners = $_POST['carbon_fields_compact_input']['_psp_partners'];
+        $product = wc_get_product($postID);
+
+        $totalPartnersComission = array_reduce($partners, function (float $total, array $partner) use ($product) {
+            $partner = new Partner([
+                'psp_comission_type' => $partner['_psp_comission_type'],
+                'psp_percentage' => $partner['_psp_percentage'] ?? 0,
+                'psp_fixed_amount' => $partner['_psp_fixed_amount'] ?? 0,
+            ]);
+
+            return $partner->calculateValue($product) + $total;
+        }, 0);
+
+        if (Helper::priceInCents($totalPartnersComission) > Helper::priceInCents((float) $product->get_price())) {
+            set_transient('psp_comission_error_' . $postID, __('Partners comissions has a bigger value than product price.', 'pagarme-split-payment'), YEAR_IN_SECONDS);
+            return;
+        }
+
+        delete_transient('psp_comission_error_' . $postID);
     }
 }
